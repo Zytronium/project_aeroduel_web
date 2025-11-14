4dbe7224c1a547eb4c6430491aff32a7a223032d
Zytronium <dastelljes@outlook.com>
2025-11-14 17:42:48 -0600

    Complete description of project in about section

--- src/app/compontents/AboutSection.tsx
+++ src/app/compontents/AboutSection.tsx
@@ -1,9 +1,30 @@
 export const AboutSection = () => {
   return (
     <section id="about" className="bg-background-2 text-foreground-2">
-      <h2 className="text-gold text-shadow-lg">About</h2>
-      <p>Lorum Ipsum blah blah blah</p>
-      <p>Cool project yes yes.</p>
+      <h2 className="text-gold text-shadow-lg">What is Aeroduel?</h2>
+      <p>
+        Aeroduel merges cutting-edge software engineering with RC model aircraft
+        to create the an advanced aerial combat system for RC aircraft.
+      </p>
+      <p>
+        We&apos;re equipping high-performance RC fighter jets with camera systems
+        that automatically detect targets equipped with Aeroduel detection lights.
+        This allows aircraft to use advanced computer vision to lock onto targets
+        and simulate aerial combat, keeping score via a Wi-Fi connection to the
+        host server back on the ground. Add FPV cameras and pilots can get a true
+        cockpit experience and aim like a pro.
+      </p>
+
+      <p>Our mobile app will allow users to register their planes equipped with
+      Aeroduel equipment and enter dogfights by scanning a QR code or entering a
+      game pin displayed on the host computer. This requires at least one player
+      to have our desktop app installed, and all players to have our mobile app
+      installed, as well as the proper Aeroduel equipment installed on their
+      plane of choice.</p>
+
+      <small>Please note that all pilots must be experienced and confident with
+      their aircraft of choice. Pilots must follow all AMA safety guidelines and
+      local laws.</small>
     </section>
   );
 };
--- src/app/compontents/HeroSection.tsx
+++ src/app/compontents/HeroSection.tsx
@@ -2,10 +2,10 @@ export function HeroSection() {
   return (
     <section id="hero">
       <div
-        className="absolute inset-0 bg-gradient-to-br from-darkernavy via-darkernavy/20 to-navy"></div>
+        className="fixed top-0 left-0 w-screen h-screen bg-gradient-to-br from-darkernavy via-darkernavy/20 to-navy"></div>
       <div className="z-1 px-4 py-2 flex flex-col items-center">
-        <h1 className="text-accent-2 text-shadow-lg">Aeroduel</h1>
-        <p>
+        <h1 className="text-accent-2 text-shadow-lg title">AERODUEL</h1>
+        <p className="subtitle">
           The future of RC aerial combat. Real FPV immersion. Real engineering.
         </p>
         <button className="btn">Learn More</button>

b09a88ae3bc9c3f3279d1b8ea771ae3aae03aacf
“Tristian <tristian.davis@atlasschool.com>
2025-11-24 12:23:40 -0600

    how it works page setup

---
+++ src/app/how-it-works/page.tsx
@@ -0,0 +1,11 @@
+import { HowItWorksSection } from "@/app/components/HowItWorksSection";
+
+export default function HowItWorksPage() {
+  return (
+    <>
+      <div className="z-1">
+        <HowItWorksSection />
+      </div>
+    </>
+  );
+}
